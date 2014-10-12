ebdfcbffcd160a69d2091037627b75cdffc60f73
xi4oh4o <xi4oh4o@gmail.com>
2014-10-12 15:11:52 +0800

    添加Faraday Gem 和 encrypt方法

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'json'
 require 'erb'
 require 'time'
 require 'yaml'
+require 'faraday'
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/flash'
@@ -16,11 +17,13 @@ helpers do
   def getConf
     @conf = YAML.load(File.open('config.yml'))
   end
+
+  def encrypt(password)
+    Digest::SHA256.hexdigest(password)
+  end
 end
 
 class Auth < ActiveRecord::Base
   validates :username, uniqueness: true
   validates :username, :password, presence: true
 end
-
-
